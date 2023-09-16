f9e2806409cd4300f7c3f80b6e33c1f8acdae062
Semen SBM <1selezov1@gmail.com>
2023-09-16 20:40:38 +0000

    2

--- edgeDriver.py
+++ edgeDriver.py
@@ -1,27 +1,67 @@
+import traceback
+
 from selenium import webdriver
-from selenium.webdriver.remote import webelement
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-import pandas as pd
-import time
-from bs4 import BeautifulSoup
-import os
-from datetime import datetime
-from selenium.webdriver import ActionChains
 
-driver = webdriver.Edge(executable_path = 'C:\\Users\\Downloads\\edgedriver_win32\\msedgedriver.exe')
-def get_trulia_estimate(address):
-    driver.get('https://www.trulia.com/')
-    print(address)
-    element = (By.ID, 'homepageSearchBoxTextInput')
+from fp.fp import FreeProxy
+from fake_headers import Headers
 
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable(element)).click()
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable(element)).send_keys(address)
 
-    search_button = (By.CSS_SELECTOR, "button[data-auto-test-id='searchButton']")
+def get_driver(proxy=False):
 
-    WebDriverWait(driver, 50).until(EC.element_to_be_clickable(search_button)).click()
+    options=webdriver.ChromeOptions()
 
-    time.sleep(3) 
+    options.add_argument('--no-sandbox')
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-extensions')
+    options.add_argument(f"user-agent={get_headers()}")
+
+    if proxy:
+        options.add_argument(
+            '--proxy-server=%s' % FreeProxy(country_id=['BR']).get().split('//')[-1]
+        )
+
+    driver = webdriver.Remote(
+        command_executor='http://selenium:4444/wd/hub',
+        options=options
+    ) 
+    return driver
+
+
+def get_headers():
+    header = Headers(
+        browser="chrome",  # Generate only Chrome UA
+        os="linux",  # Generate only Windows platform
+        headers=False # generate misc headers
+    )
+    return header.generate()['User-Agent']
+
+
+def check_proxy(driver):
+    
+    driver.get('http://www.whatismyproxy.com/')
+    proxy_check = driver.find_element(By.XPATH, '//div[@class="information"]')
+    
+    print(proxy_check.text)
+
+
+def main():
+
+    driver = get_driver(proxy=True)
+
+    try:
+        check_proxy(driver)
+        print("\_ That`s it _/")
+
+    except Exception:
+        print(traceback.format_exc())
+
+    finally:
+        driver.close()
+        driver.quit()
+
+
+if __name__ == "__main__":
+    main()
